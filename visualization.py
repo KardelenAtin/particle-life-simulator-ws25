369dddf819fb4b6effd83c4de68ef8151c31ebac
Melina Tsigka <melina.tsigka@study.hs-duesseldorf.de>
2025-12-15 21:44:03 +0100

    GUI Window created

--- visualization.py
+++ visualization.py
@@ -1,1 +1,12 @@
+from vispy import app, scene
+import numpy as np
 
+canvas = scene.SceneCanvas(show=True)
+canvas.size = 800, 600
+canvas.title = "GUI Window"
+
+view = canvas.central_widget.add_view()
+view.camera = scene.cameras.PanZoomCamera(aspect=1)
+
+if __name__ == '__main__':
+    app.run()
