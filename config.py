1b6f86af482cba1134c5f9f136803f05299dbd4a
Vipusiny Vijayakumar <vipusiny.vijayakumar@study.hs-duesseldorf.de>
2025-12-16 17:36:02 +0100

    added friction, delta_t, MAX_RADIUS

--- config.py
+++ config.py
@@ -12,13 +12,18 @@ SPACE_MAX = 10.0
 # number of particle types
 N_PARTICLE_TYPES = 4
 
+
 # colors for each particle type (RGBA)
 COLOR_DISTRIBUTION = np.array([
     [1.0, 0.0, 0.0],  # Red
     [0.0, 1.0, 0.0],  # Green
-    [0.0, 0.0, 1.0],  # Blue
-    [1.0, 1.0, 0.0],  # Yellow
+    [0.0, 1.0, 0.0],  # Blue
+    [0.0, 1.0, 0.0],  # Yellow
 ])
+
+
+
+
 
 friction = 0.99
 delta_t = 0.5
