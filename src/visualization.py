4bf410c4331ede173825ce932411a1c5c62d07cd
Melina Tsigka <melinatsigka@hotmail.com>
2026-02-15 20:28:18 +0100

    Fix refactoring into main()

--- src/visualization.py
+++ src/visualization.py
@@ -3,14 +3,16 @@ import numpy as np
 from src.simulation import Simulation
 from src.config import SPACE_MIN, SPACE_MAX
 
+
 def main():
     sim = Simulation([
         {"n":500, "type": 0}, 
         {"n":500, "type": 1}, 
         {"n":500, "type": 2}, 
-        {"n":500, "type": 3}])
+        {"n":500, "type": 3}
+    ])
 
-#GUI window
+    # GUI window
     canvas = scene.SceneCanvas(keys="interactive", show=True)
     canvas.size = 800, 600
     canvas.title = "Particles"
@@ -20,30 +22,30 @@ def main():
     view.camera.set_range(
         x=(SPACE_MIN, SPACE_MAX),
         y=(SPACE_MIN, SPACE_MAX),
-        margin=0)
+        margin=0
+    )
 
-
-#scatter plot
     scatter = scene.visuals.Markers()
     scatter.set_gl_state('translucent', blend=True, depth_test=False)
     scatter.set_data(
         sim.positions.astype(np.float32),
-        face_color = sim.colors.astype(np.float32),
-        size = 6
+        face_color=sim.colors.astype(np.float32),
+        size=6
     )
     view.add(scatter)
-
 
     def update(event):
         sim.step()
         scatter.set_data(
             sim.positions.astype(np.float32),
-            face_color = sim.colors.astype(np.float32))
+            face_color=sim.colors.astype(np.float32),
+        )
 
     timer = app.Timer(interval=1/60, connect=update, start=True)
-    timer.start()
 
-if __name__ == "__main__":
+    app.run()
+
+
+if __name__ == "_main_":
     main()
-
 
